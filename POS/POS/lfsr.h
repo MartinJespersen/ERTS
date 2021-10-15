dc16b9327ce27628bb8f64a9c962e599292cc0d3
Tristan Moeller <Trilleo@hotmail.com>
2021-10-15 10:51:39 +0200

    changed to uint16_t

--- POS/POS/lfsr.h
+++ POS/POS/lfsr.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <random>
+#include "fixed_point.h"
 
 // Inspiration from: https://highlevel-synthesis.com/2017/02/10/lfsr-in-hls/
 class lfsr
@@ -7,7 +8,8 @@ class lfsr
 public:
 	double generate_random(double from, double to); 
 private:
-	unsigned int pseudo_random(unsigned int seed, int load);
+	unsigned int pseudo_random(unsigned
+		int seed, int load);
 	double uniform_random(double from, double to); 
 };
 
