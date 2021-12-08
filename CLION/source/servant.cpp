bbca1b3843b821dec8306b20a73e75db281254f5
MartinJespersen <36986468+MartinJespersen@users.noreply.github.com>
2021-12-08 14:12:25 +0100

    rest

--- CLION/source/servant.cpp
+++ CLION/source/servant.cpp
@@ -3,13 +3,6 @@
 //
 
 #include "../headers/servant.h"
-#ifdef __unix__
-# include <unistd.h>
-#elif defined _WIN32
-#include <windows.h>
-#define sleep(x) Sleep(1000 * (x))
-#endif
-
 
 const bool servant::is_running() const
 {
@@ -28,7 +21,6 @@ message servant::get_global_best_sw()
 
 message servant::get_global_best_hw()
 {
-    sleep(5);
     std::cout << __func__ << std::endl;
     float i[N] = {1,1};
     return message(i);
